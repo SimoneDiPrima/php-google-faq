c1d36c9db3e9a5407a78617891c78a8ad1d917a6
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-08-31 15:32:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$myArray=[
+    ['<strong>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum<strong>'=>
+    ['faq1'=>'paragraph1',
+        'faq2'=>'paragraph1',
+        'faq3'=>'paragraph1',
+        'faq4'=>'paragraph1',
+        'faq5'=>'paragraph1',]
+]
+];
+var_dump($myArray);
+?>
