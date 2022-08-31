bfe53bb7ad80ce2eb8777d27aea10d3cf21bd358
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-08-31 16:21:10 +0200

    uso ciclo foreach

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
 <?php
 $myArray=[
-    ['<strong>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum<strong>'=>
+    '<strong>Come state implementando la recente decisione della Corte di giustizia dell<a href="">Unione europea (CGUE) relativa al diritto all oblio</a>'=>
     ['faq1'=>'paragraph1',
         'faq2'=>'paragraph1',
         'faq3'=>'paragraph1',
         'faq4'=>'paragraph1',
-        'faq5'=>'paragraph1',]
-]
+        'faq5'=>'paragraph1',],
+        '<strong>Come state implementando la recente decisione della Corte di giustizia dell<a href="">Unione europea (CGUE) relativa al diritto all oblio</a>'=>
+        ['faq1'=>'paragraph1',
+            'faq2'=>'paragraph1',
+            'faq3'=>'paragraph1',
+            'faq4'=>'paragraph1',
+            'faq5'=>'paragraph1',],
+            '<strong>Come state implementando la recente decisione della Corte di giustizia dell<a href="">Unione europea (CGUE) relativa al diritto all oblio</a>'=>
+            ['faq1'=>'paragraph1',
+                'faq2'=>'paragraph1',
+                'faq3'=>'paragraph1',
+                'faq4'=>'paragraph1',
+                'faq5'=>'paragraph1',],
+                '<strong>Come state implementando la recente decisione della Corte di giustizia dell<a href="">Unione europea (CGUE) relativa al diritto all oblio</a>'=>
+                ['faq1'=>'paragraph1',
+                    'faq2'=>'paragraph1',
+                    'faq3'=>'paragraph1',
+                    'faq4'=>'paragraph1',
+                    'faq5'=>'paragraph1',],
+            
 ];
-var_dump($myArray);
+foreach($myArray as $question => $answer){
+    foreach($answer as $value){
+    }
+    
 ?>
+<h2><?php echo $question ?></h2>
+<h4><?php echo $value ?></h4>
+<?php }?>
