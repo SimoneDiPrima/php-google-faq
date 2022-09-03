e9f04cf610b29ca5868f065aab422da27d19ae6a
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-03 14:37:40 +0200

    aggiunta footer e modifiche font e layout

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 $myArray=[
-    '<strong>Come state implementando la recente decisione della Corte di giustizia dell`Unione europea (CGUE) relativa al diritto all oblio'=>
+    '<strong>Come state implementando la recente decisione della Corte di giustizia dell`Unione europea (CGUE) relativa al diritto all oblio</strong>'=>
     ['La recente <a href="www.google.it">decisione della Corte di giustizia dell Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.',
         'Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.',
         'Per presentare una richiesta di rimozione, compila questo <a href="www.google.it">modulo web</a>. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po di tempo perché abbiamo già ricevuto tante richieste simili. Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d accordo con la nostra valutazione, puoi rivolgerti all Autorità garante per la protezione dei dati personali nel tuo paese.',
         'Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.',
         'Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione.',],
-        '<strong>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?'=>
+        '<strong>Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?</strong>'=>
         [
         'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.',
         'Siamo costantemente al lavoro per garantire un elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.',
@@ -14,7 +14,7 @@ $myArray=[
         '<a href="www.google.it">Scopri</a> quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.',
         
         ],
-            '<strong>Perché il mio account è associato a un paese?<strong>'=>
+            '<strong>Perché il mio account è associato a un paese?</strong>'=>
             ['Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:',
             '1. La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
             <ul>
@@ -49,80 +49,48 @@ $myArray=[
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <title>Document</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+        <link rel="stylesheet" href="./css/style.css">
+        <title>Google Privacy Policies</title>
        </head>
        <body>
-        <header class="p-3 d-flex flex-column align-items-start">
-            <h2 class="col-6 ms-30"><a href="www.google.it">GOOGLE</a> Privacy&Termini</h2>
-            <ul class="d-flex-bottom col-6 justify-content-between color-gray">
-                <li class=""><a href="">Introduzione</a></li>
-                <li class=""><a href="">Norme sulla privacy</a></li>
-                <li class=""><a href="">Termini di Servizio</a></li>
-                <li class=""><a href="">Tecnologie</a></li>
-                <li class="active"><a href="">Domande Frequenti</a></li>
-           </ul>
-        </header>
-        <div class="offset-1 col-10 mt-4">
-        <?php foreach($myArray as $title => $paragraph) : ?>
-            <h2> <?php echo $title ?> </h2>
-            <?php foreach($paragraph as $value) :?>
-                
-                    <p><?php echo $value ?>
-            </p>
-             
-            <?php endforeach;?>
-        <?php endforeach;?>
-
-        </div>
-      
-        <style>
-            *{
-                font-family: 'Roboto', sans-serif;
-            }
-            a{text-decoration:none;
-            color:dodgerblue;}
-            .text-center {
-                text-align:center;
-            }
-            header{
-                height:100px;
-                background-color: lightgray;
-                border-bottom:2px solid dodgerblue;
-            }
-            .d-flex-bottom{
-                display:flex;
-               justify-content:flex-start;
-            }
-            .ms-30{
-            margin-left:30px;
-            }
-            .pt-20{padding-top:20px;}
-            .pt-10{
-                padding-top:10px;
-            }
-            .active:hover{
-                cursor:pointer;
-                text-decoration: underline 5px dodgerblue;
-            }
-            .margin-right-20{
-                margin-right:20px;
-            }
-            ul li{
-                list-style-type: none;
-                font-weight:bolder;
-            }
-            .w-80{width:80%;}
-            .color-gray{
-                color:rgb(60, 64, 67);}
-           
-
-            </style>
-
-        </div>
-       
+            <header class="p-3 d-flex flex-column align-items-start">
+                <h2 class="col-6 ms-30"><a href="www.google.it">GOOGLE</a> Privacy&Termini</h2>
+                <ul class="d-flex col-6 justify-content-between color-gray">
+                    <li class=""><a href="www.google.it">Introduzione</a></li>
+                    <li class=""><a href="www.google.it">Norme sulla privacy</a></li>
+                    <li class=""><a href="www.google.it">Termini di Servizio</a></li>
+                    <li class=""><a href="www.google.it">Tecnologie</a></li>
+                    <li class="active"><a href="www.google.it">Domande Frequenti</a>
+                    </li>
+            </ul>
+            </header>
+            <div class="offset-1 col-10 mt-4">
+                <?php foreach($myArray as $title => $paragraph) : ?>
+                <h2> <?php echo $title ?> </h2>
+                <?php foreach($paragraph as $value) :?>  
+                <p><?php echo $value ?></p>
+                <?php endforeach;?>
+                <?php endforeach;?>
+            </div>
+            <footer>
+                <div class="color-gray offset-1 col-10 d-flex mt-5 justify-content-between align-items-center">
+                    <ul class="d-flex padding-left-0 lh-100">
+                        <li class="me-1">Google</li>
+                        <li class="me-1">Tutto su Google</li>
+                        <li class="me-1">Privacy</li>
+                        <li class="me-1">Termini</li>
+                    </ul>
+                    <select name="" class="lh-100">
+                        <option value="Italiano" class="text-capitalize">Italiano</option>
+                        <option value="English"  class="text-capitalize">english</option>
+                        <option value="Espanòl"  class="text-capitalize">espanòl</option>
+                        <option value="Chinese"  class="text-capitalize">chinese</option>
+                    </select>
+                </div>
+            </footer>
        </body>
        </html>
     
